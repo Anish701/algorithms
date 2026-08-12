19697131a534f7777ba6f9582d31e3ee7e709dc1
Anish701 <anish2sinha@gmail.com>
2026-08-12 16:34:28 -0400

    feat: add arrays-hashing dir
    Signed-off-by: Anish701 <anish2sinha@gmail.com>

---
+++ arrays-hashing/contains_duplicates.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        st = set()
+
+        for n in nums:
+            if n in st:
+                return True
+            st.add(n)
+        
+        return False
