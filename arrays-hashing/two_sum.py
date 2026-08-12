8c17e732fb2f55d50b71dc1204a86ee02f45c74b
Anish701 <anish2sinha@gmail.com>
2026-08-12 17:05:17 -0400

    feat: group anagram detection
    Signed-off-by: Anish701 <anish2sinha@gmail.com>

---
+++ arrays-hashing/two_sum.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in mp:
+                return [mp[diff], i]
+
+            mp[nums[i]] = i
+        
+        return [-1, -1]
